2941546eed2b4e1b516b80f97e33e47ba0705393
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-03-02 03:54:36 -0300

    trabalhando com post

--- Express/index.js
+++ Express/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+const somar = (num1, num2) => {
+	return num1 + num2;
+};
 
 app.get("/api/:id", (req, res) => {
+	res.send("Hello World: " + req.params.id); //Enviando um req.params
+});
 
-res.send('Hello World: ' + req.params.id) //Enviando um req.params 
- 
+app.post("/somar", (req, res) => {
+	const { num1, num2 } = req.body;
+	const result = somar(num1, num2);
+	res.status(200).send({result});
 });
 
 app.listen(port, () => {
