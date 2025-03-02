466657ec686df64466b8f0cad68b1a98ccde41e8
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-03-02 03:08:33 -0300

    Eviando um query params pela URL

--- Express/index.js
+++ Express/index.js
@@ -2,5 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => {
+
+res.send('Hello World: ' + req.query.num) //Enviando um query params 
+ 
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`);
+});
