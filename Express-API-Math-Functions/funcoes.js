c198e4e3fd004158c87f92dc5b575dc9f0be5d99
RafaelBatistoti <rafael.batistoti.luiz@gmail.com>
2025-03-03 16:12:53 -0300

    Criando um Debug e tratand os erros con msg de erros personalizadas

--- Express-API-Math-Functions/funcoes.js
+++ Express-API-Math-Functions/funcoes.js
@@ -1,16 +1,27 @@
 const somar = (num1, num2) => {
+	if (isNaN(num1) || isNaN(num2))
+		throw new Error("invalid!! Inform only numbers!!");
+
 	return num1 + num2;
 };
 
 const subtrair = (num1, num2) => {
+	if (isNaN(num1) || isNaN(num2))
+		throw new Error("invalid!! Inform only numbers!!");
 	return num1 - num2;
 };
 
 const multiplicar = (num1, num2) => {
+	if (isNaN(num1) || isNaN(num2))
+		throw new Error("invalid!! Inform only numbers!!");
 	return num1 * num2;
 };
 
 const dividir = (num1, num2) => {
+	if (isNaN(num1) || isNaN(num2))
+		throw new Error("invalid!! Inform only numbers!!");
+
+	if (num1 == 0 && num2 == 0) throw new Error("Is not divid by zero ('0')");
 	return num1 / num2;
 };
 
--- Express-API-Math-Functions/opracoes.js
+++ Express-API-Math-Functions/opracoes.js
@@ -1,31 +1,47 @@
 const { somar, subtrair, multiplicar, dividir } = require("./funcoes");
 
 function apiSomar(req, res) {
-	const { num1, num2 } = req.body;
-	const result = somar(num1, num2);
-	res.status(200).send({ result });
+	try {
+		const { num1, num2 } = req.body;
+		const result = somar(num1, num2);
+		res.status(200).send({ result });
+	} catch (e) {
+		res.status(500).send({ msg: e.message });
+	}
 }
 
 function apiSubtrair(req, res) {
-	const { num1, num2 } = req.body;
-	const result = subtrair(num1, num2);
-	res.status(200).send({ result });
+	try {
+		const { num1, num2 } = req.body;
+		const result = subtrair(num1, num2);
+		res.status(200).send({ result });
+	} catch (e) {
+		res.status(500).send({ msg: e.message });
+	}
 }
 
 function apiMultiplicar(req, res) {
-	const { num1, num2 } = req.body;
-	const result = multiplicar(num1, num2);
-	res.status(200).send({ result });
+	try {
+		const { num1, num2 } = req.body;
+		const result = multiplicar(num1, num2);
+		res.status(200).send({ result });
+	} catch (e) {
+		res.status(500).send({ msg: e.message });
+	}
 }
 
 function apiDividir(req, res) {
-	const { num1, num2 } = req.body;
-	const result = dividir(num1, num2);
-	res.status(200).send({ result });
+	try {
+		const { num1, num2 } = req.body;
+		const result = dividir(num1, num2);
+		res.status(200).send({ result });
+	} catch (e) {
+		res.status(500).send({ msg: e.message });
+	}
 }
 
-function hello (req, res) {
+function hello(req, res) {
 	res.send("Hello World: " + req.params.id); //Enviando um req.params
-};
+}
 
-module.exports = { apiSomar, apiSubtrair, apiMultiplicar, apiDividir, hello};
+module.exports = { apiSomar, apiSubtrair, apiMultiplicar, apiDividir, hello };
